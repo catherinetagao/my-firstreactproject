b647112551bc437917c970a7ffb4ed84d3399ffc
Cath Tagao <katetagaoe@gmail.com>
2024-07-13 17:50:36 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App({content, content2}) {
     <>
       <h1>{content}</h1>
       <h2>{content2}</h2>
+      <img src="https://images.unsplash.com/photo-1687246493079-1361abc6d875?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
     </>
   )
 }
