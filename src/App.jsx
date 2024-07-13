cb6812cefb92a8568e6c7e08c94145ac2af4888e
Cath Tagao <katetagaoe@gmail.com>
2024-07-13 17:40:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App({content, content2}) {
+
+  return (
+    <>
+      <h1>{content}</h1>
+      <h2>{content2}</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App content={'Hello'} content2={"Hello"} />
+    <App content={'World'}  />
+  </React.StrictMode>,
+)
